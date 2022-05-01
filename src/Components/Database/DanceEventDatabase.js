3f5805e04f14d51323b8fb077257aaee02972682
darkknight20032001 <84170745+darkknight20032001@users.noreply.github.com>
2022-05-01 06:14:26 +0530

    committed

---
+++ src/Components/Database/DanceEventDatabase.js
@@ -0,0 +1,52 @@
+import dance_logo from "../assets/Images/dance_logo.jpg";
+const DanceEventDatabase = [
+  {
+    cityName: "Ranchi",
+    eventName: "JSCA Dance Championship",
+    eventImage: dance_logo,
+    eventDescription: "Dance and rock the world",
+    eventDate: "21/05/2022",
+    eventPrize: "Rs. 20000 for winner and a free air travel to UAE",
+  },
+  {
+    cityName: "Lucknow",
+    eventName: "Tata Dance Championship",
+    eventImage: dance_logo,
+    eventDescription: "Dance and emote",
+    eventDate: "24/05/2022",
+    eventPrize: "Rs. 40000 for winner and a Rolex Watch ",
+  },
+  {
+    cityName: "Delhi",
+    eventName: "DTDC Dance Competition",
+    eventImage: dance_logo,
+    eventDescription: "Dance to Shine",
+    eventDate: "01/06/2022",
+    eventPrize: "Rs. 1L for winner and a free air travel to Las Vegas",
+  },
+  {
+    cityName: "Mumbai",
+    eventName: "Little Masters Dance Championship",
+    eventImage: dance_logo,
+    eventDescription: "Anybody can dance",
+    eventDate: "05/06/2022",
+    eventPrize: "Rs. 2.5L for winner and free entry to Carnival Dance Troupe",
+  },
+  {
+    cityName: "Chennai",
+    eventName: "Glitz Lighting Dance Battle",
+    eventImage: dance_logo,
+    eventDescription: "Dance to Express",
+    eventDate: "15/06/2022",
+    eventPrize: "Rs. 1.2L for winner and air ticket to Moscow",
+  },
+  {
+    cityName: "Kolkata",
+    eventName: "Splits Dance Masters",
+    eventImage: dance_logo,
+    eventDescription: "Start, Repeat and Dance",
+    eventDate: "30/06/2022",
+    eventPrize: "Rs. 3L for winner and free entry to Salt Lake",
+  },
+];
+export default DanceEventDatabase;
