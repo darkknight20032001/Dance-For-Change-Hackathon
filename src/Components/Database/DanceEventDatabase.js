6c92bc7e06e48fe676667dd9cda98b3994de3dc7
Suraj Poddar <surajku2022@gmail.com>
2022-05-01 12:09:57 +0530

    commit

--- src/Components/Database/DanceEventDatabase.js
+++ src/Components/Database/DanceEventDatabase.js
@@ -3,7 +3,7 @@ const DanceEventDatabase = [
   {
     cityName: "Ranchi",
     eventName: "JSCA Dance Championship",
-    eventImage: dance_logo,
+    eventImage: "https://i.pinimg.com/originals/44/df/f5/44dff5473cf8394c3ca3d2eb6c02f98f.png",
     eventDescription: "Dance and rock the world",
     eventDate: "21/05/2022",
     eventPrize: "Rs. 20000 for winner and a free air travel to UAE",
@@ -11,7 +11,7 @@ const DanceEventDatabase = [
   {
     cityName: "Lucknow",
     eventName: "Tata Dance Championship",
-    eventImage: dance_logo,
+    eventImage: "https://img.freepik.com/free-vector/dance-logo_533573-174.jpg?w=2000",
     eventDescription: "Dance and emote",
     eventDate: "24/05/2022",
     eventPrize: "Rs. 40000 for winner and a Rolex Watch ",
@@ -19,7 +19,7 @@ const DanceEventDatabase = [
   {
     cityName: "Delhi",
     eventName: "DTDC Dance Competition",
-    eventImage: dance_logo,
+    eventImage: "https://images-platform.99static.com//mg6tNfsrYgcmo0W13CSe5VUcmrQ=/1023x17:1937x931/fit-in/500x500/99designs-contests-attachments/99/99425/attachment_99425353",
     eventDescription: "Dance to Shine",
     eventDate: "01/06/2022",
     eventPrize: "Rs. 1L for winner and a free air travel to Las Vegas",
@@ -27,7 +27,7 @@ const DanceEventDatabase = [
   {
     cityName: "Mumbai",
     eventName: "Little Masters Dance Championship",
-    eventImage: dance_logo,
+    eventImage: "https://i.pinimg.com/originals/fb/aa/98/fbaa983052d78e3384c2ec49f907ec9a.jpg",
     eventDescription: "Anybody can dance",
     eventDate: "05/06/2022",
     eventPrize: "Rs. 2.5L for winner and free entry to Carnival Dance Troupe",
@@ -43,7 +43,7 @@ const DanceEventDatabase = [
   {
     cityName: "Kolkata",
     eventName: "Splits Dance Masters",
-    eventImage: dance_logo,
+    eventImage: "https://st.depositphotos.com/1027431/4657/v/450/depositphotos_46573749-stock-illustration-abstract-vector-logo-design-template.jpg",
     eventDescription: "Start, Repeat and Dance",
     eventDate: "30/06/2022",
     eventPrize: "Rs. 3L for winner and free entry to Salt Lake",
